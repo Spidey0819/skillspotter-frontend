1365d9af7fceee257354ce8e6c8304e009959363
Spidey0819 <goswamidhruv302031@gmail.com>
2025-04-08 14:31:50 -0300

    initial commit

---
+++ public/Config.js
@@ -0,0 +1,8 @@
+// This file gets loaded before React starts
+window.APP_CONFIG = {
+    apiConfigPath: '/api-config',
+    region: 'us-east-1', // Will be replaced dynamically
+    resumeBucket: 'skillspotter-resumes', // Will be replaced dynamically
+    jobDescBucket: 'skillspotter-jobdesc', // Will be replaced dynamically
+    environment: 'production'
+};
